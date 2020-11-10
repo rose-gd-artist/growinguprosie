bd796971b390e9a7f6b3f07ddc88785cdc6022ff
rose-gd-artist <61298013+rose-gd-artist@users.noreply.github.com>
2020-11-10 00:51:29 -0500

    working on db.json calls in mainContent

--- src/Header/index.js
+++ src/Header/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import './index.css';
 import './images/gup_logo.svg';
 import './images/gup_logo_text.svg';
@@ -12,20 +12,33 @@ import './images/contactButton.svg';
 import './images/shopCart.svg';
 
 
-const Header = () => {
+const Header = (props) => {
+
+    const [responseShopOwner, shopOwnerInfo] = useState([]);
+
+    useEffect(() => {
+        const getJsonOwnerData = async () => {
+            const response = await fetch(`http://www.localhost:3000/shopOwner/1`);
+            const responseShopOwner = await response.json();
+            shopOwnerInfo(responseShopOwner);
+        };
+        getJsonOwnerData();
+    }, []);
+
+
     return (
         <div className="appHeader">
             <div className="logoLeft">
                 <img src={window.location.origin + '/images/gup_logo.svg'} alt="growing up rosie logo" className="circleLogo"/>
                 <p className="shopOwnerTitle">SHOP OWNER</p>
-                <p className="shopOwnerName">Rosemary Meyers</p>
+                <p className="shopOwnerName">{responseShopOwner.name}</p>
                 <img src={window.location.origin + '/images/contactButton.svg'} alt="contact button" className="contactButton"/>
             </div>
             <div className="headerLeftSide">
 
                 <img src={window.location.origin + '/images/gup_logo_text.svg'} alt="growing up rosie logo" className="textLogo"/>
                 <div className="locationLine"><img src={window.location.origin + '/images/mapMarker.svg'} alt="map marker for city, state" className="mapMarker"/>
-                <p className="cityState">Philadelphia, Pennsylvania</p></div>
+                <p className="cityState">{responseShopOwner.shopLocation}</p></div>
 
                 <div className="salesLine">
                     <p className="saleNumber">0 Sales</p>
@@ -54,5 +67,48 @@ const Header = () => {
         </div>
     );
 };
+
+// const Header = () => {
+//     return (
+//         <div className="appHeader">
+//             <div className="logoLeft">
+//                 <img src={window.location.origin + '/images/gup_logo.svg'} alt="growing up rosie logo" className="circleLogo"/>
+//                 <p className="shopOwnerTitle">SHOP OWNER</p>
+//                 <p className="shopOwnerName">Rosemary Meyers</p>
+//                 <img src={window.location.origin + '/images/contactButton.svg'} alt="contact button" className="contactButton"/>
+//             </div>
+//             <div className="headerLeftSide">
+
+//                 <img src={window.location.origin + '/images/gup_logo_text.svg'} alt="growing up rosie logo" className="textLogo"/>
+//                 <div className="locationLine"><img src={window.location.origin + '/images/mapMarker.svg'} alt="map marker for city, state" className="mapMarker"/>
+//                 <p className="cityState">Philadelphia, Pennsylvania</p></div>
+
+//                 <div className="salesLine">
+//                     <p className="saleNumber">0 Sales</p>
+//                 </div>
+
+//                 <div className="starsLine">
+//                     <div className="averageStarRating">
+//                         <img src={window.location.origin + '/images/emptyStar.svg'} alt="1 Empty star" className="starEmpty"/>
+//                         <img src={window.location.origin + '/images/emptyStar.svg'} alt="1 Empty star" className="starEmpty"/>
+//                         <img src={window.location.origin + '/images/emptyStar.svg'} alt="1 Empty star" className="starEmpty"/>
+//                         <img src={window.location.origin + '/images/emptyStar.svg'} alt="1 Empty star" className="starEmpty"/>
+//                         <img src={window.location.origin + '/images/emptyStar.svg'} alt="1 Empty star" className="starEmpty"/>
+//                     </div>
+//                     <p className="totalReviewsTally">(0)</p>
+//                 </div>
+
+//                 <div className="faveShop">
+//                     <img src={window.location.origin + '/images/unFaveShop.svg'} alt="Click heart to make this your favorite shop" className="faveShopOpenHeart"/>
+//                     <p className="faveShopTally">Favorite Shop (0)</p>
+//                 </div>
+//             </div>
+
+//             <div className="headerRightSide">
+//                 <img src={window.location.origin + '/images/shopCart.svg'} alt="shopping cart" className="shopCart"/>
+//             </div>
+//         </div>
+//     );
+// };
 
 export default Header;
--- src/MainContent/index.js
+++ src/MainContent/index.js
@@ -1,48 +1,108 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import './index.css';
 
-const MainContent = () => {
+const MainContent = (props) => {
+
+    const [responseShopItem, shopItemInfo] = useState([]);
+
+    useEffect(() => {
+        const getItemData = async () => {
+            const response2 = await fetch(`http://www.localhost:3000/shopOwner/1/shopItem`);
+            const responseShopItem = await response2.json();
+            shopItemInfo(responseShopItem);
+        };
+        getItemData();
+    }, []);
+
     return (
         <div className="contentPage">
-            <div className="indexItem">
+  
+                {responseShopItem.forEach((item) => {
+
+                    <div className="indexItem">
+                        <img src={window.location.origin + '/images/10FingersPic.jpg'} alt="10 Fingers 10 Toes I'm Good Onesie" className="indexPic"/>
+                        <p className="indexItemType">itemType={item.itemType}</p>
+                        <p className="indexItemDetail">name={item.name}</p>
+                        <p className="indexItemPrice">price={item.price}</p>
+                    </div>
+                })}
+    
+            {/* {responseShopItem.forEach((item) => {
+
+                <div className="indexItem">
+                    <img src={window.location.origin + '/images/10FingersPic.jpg'} alt="10 Fingers 10 Toes I'm Good Onesie" className="indexPic"/>
+                    <p className="indexItemType">itemType={item.itemType}</p>
+                    <p className="indexItemDetail">name={item.name}</p>
+                    <p className="indexItemPrice">price={item.price}</p>
+                </div>
+
+            })} */}
+            {/* <div className="indexItem">
                 <img src={window.location.origin + '/images/10FingersPic.jpg'} alt="10 Fingers 10 Toes I'm Good Onesie" className="indexPic"/>
-                <p className="indexItemType">Infant Onesie</p>
-                <p className="indexItemDetail">10 Fingers 10 Toes I'm Good</p>
-                <p className="indexItemPrice">$15.00</p>
-            </div>
-            <div className="indexItem">
-                <img src={window.location.origin + '/images/10FingersPic.jpg'} alt="1o Fingers 10 Toes I'm Good Onesie" className="indexPic"/>
-                <p className="indexItemType">Infant Onesie</p>
-                <p className="indexItemDetail">10 Fingers 10 Toes I'm Good</p>
-                <p className="indexItemPrice">$15.00</p>
-            </div>
-            <div className="indexItem">
-                <img src={window.location.origin + '/images/10FingersPic.jpg'} alt="1o Fingers 10 Toes I'm Good Onesie" className="indexPic"/>
-                <p className="indexItemType">Infant Onesie</p>
-                <p className="indexItemDetail">10 Fingers 10 Toes I'm Good</p>
-                <p className="indexItemPrice">$15.00</p>
-            </div>
-            <div className="indexItem">
-                <img src={window.location.origin + '/images/10FingersPic.jpg'} alt="10 Fingers 10 Toes I'm Good Onesie" className="indexPic"/>
-                <p className="indexItemType">Infant Onesie</p>
-                <p className="indexItemDetail">10 Fingers 10 Toes I'm Good</p>
-                <p className="indexItemPrice">$15.00</p>
-            </div>
-            <div className="indexItem">
-                <img src={window.location.origin + '/images/10FingersPic.jpg'} alt="1o Fingers 10 Toes I'm Good Onesie" className="indexPic"/>
-                <p className="indexItemType">Infant Onesie</p>
-                <p className="indexItemDetail">10 Fingers 10 Toes I'm Good</p>
-                <p className="indexItemPrice">$15.00</p>
-            </div>
-            <div className="indexItem">
-                <img src={window.location.origin + '/images/10FingersPic.jpg'} alt="1o Fingers 10 Toes I'm Good Onesie" className="indexPic"/>
-                <p className="indexItemType">Infant Onesie</p>
-                <p className="indexItemDetail">10 Fingers 10 Toes I'm Good</p>
-                <p className="indexItemPrice">$15.00</p>
-            </div>
+                <p className="indexItemType">{responseShopItem.itemType}</p>
+                <p className="indexItemDetail">{responseShopItem.name}</p>
+                <p className="indexItemPrice">{responseShopItem.price}</p>
+            </div> */}
         </div>
 
     );
 };
+
+
+// const MainContent = (props) => {
+
+//     const [responseShopItem, shopItemInfo] = useState([]);
+
+//     useEffect(() => {
+//         const getItemData = async () => {
+//             const response2 = await fetch(`http://www.localhost:3000/shopOwner/1/shopItem`);
+//             const responseShopItem = await response2.json();
+//             shopItemInfo(responseShopItem);
+//         };
+//         getItemData();
+//     }, []);
+
+//     return (
+//         <div className="contentPage">
+//             <div className="indexItem">
+//                 <img src={window.location.origin + '/images/10FingersPic.jpg'} alt="10 Fingers 10 Toes I'm Good Onesie" className="indexPic"/>
+//                 <p className="indexItemType">{responseShopItem.itemType}</p>
+//                 <p className="indexItemDetail">{responseShopItem.name}</p>
+//                 <p className="indexItemPrice">{responseShopItem.price}</p>
+//             </div>
+//             <div className="indexItem">
+//                 <img src={window.location.origin + '/images/10FingersPic.jpg'} alt="1o Fingers 10 Toes I'm Good Onesie" className="indexPic"/>
+//                 <p className="indexItemType">Infant Onesie</p>
+//                 <p className="indexItemDetail">10 Fingers 10 Toes I'm Good</p>
+//                 <p className="indexItemPrice">$15.00</p>
+//             </div>
+//             <div className="indexItem">
+//                 <img src={window.location.origin + '/images/10FingersPic.jpg'} alt="1o Fingers 10 Toes I'm Good Onesie" className="indexPic"/>
+//                 <p className="indexItemType">Infant Onesie</p>
+//                 <p className="indexItemDetail">10 Fingers 10 Toes I'm Good</p>
+//                 <p className="indexItemPrice">$15.00</p>
+//             </div>
+//             <div className="indexItem">
+//                 <img src={window.location.origin + '/images/10FingersPic.jpg'} alt="10 Fingers 10 Toes I'm Good Onesie" className="indexPic"/>
+//                 <p className="indexItemType">Infant Onesie</p>
+//                 <p className="indexItemDetail">10 Fingers 10 Toes I'm Good</p>
+//                 <p className="indexItemPrice">$15.00</p>
+//             </div>
+//             <div className="indexItem">
+//                 <img src={window.location.origin + '/images/10FingersPic.jpg'} alt="1o Fingers 10 Toes I'm Good Onesie" className="indexPic"/>
+//                 <p className="indexItemType">Infant Onesie</p>
+//                 <p className="indexItemDetail">10 Fingers 10 Toes I'm Good</p>
+//                 <p className="indexItemPrice">$15.00</p>
+//             </div>
+//             <div className="indexItem">
+//                 <img src={window.location.origin + '/images/10FingersPic.jpg'} alt="1o Fingers 10 Toes I'm Good Onesie" className="indexPic"/>
+//                 <p className="indexItemType">Infant Onesie</p>
+//                 <p className="indexItemDetail">10 Fingers 10 Toes I'm Good</p>
+//                 <p className="indexItemPrice">$15.00</p>
+//             </div>
+//         </div>
+
+//     );
+// };
 
 export default MainContent;
